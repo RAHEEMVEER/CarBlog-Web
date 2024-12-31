23a7a970ee2581391179254a71edeaba9b596e63
Abdul Raheem <raheemveer5@gmail.com>
2025-01-01 02:23:52 +0500

    create responsive dynamic blog and header

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -6,19 +6,17 @@ export default function page() {
       <div className="w-full flex justify-center items-center"></div>
       <div className="flex justify-center">
         <div className="w-full md:w-[75%]">
-          {/* About us */}
+          
           <div>
             <h1 className="text-lg sm:text-xl font-semibold">About Us !</h1>
             <p className="mt-2 text-sm sm:text-base">Welcome to "Car Dealing Experience", where innovation meets excellence. Our platform is dedicated to delivering an unmatched experience for our customers by offering With a deep commitment to quality and customer satisfaction, we strive to make every interaction seamless, efficient, and enjoyable.</p>
           </div>
 
-          {/* Our Mission */}
           <div className="mt-6 sm:mt-8">
             <h1 className="text-lg sm:text-xl font-semibold">Our Mission:</h1>
             <p className="mt-2 text-sm sm:text-base">At "Car Dealing Experience", our mission is simple yet impactful: to revolutionize the way is experienced. We aim to provide products and services that not only meet but exceed our customers' expectations, ensuring they always find value in choosing us.</p>
           </div>
 
-          {/* our Goal */}
           <div className="mt-6 sm:mt-8">
             <h1 className="text-lg sm:text-xl font-semibold">Our Goals:</h1>
             <ol className="list-decimal list-inside space-y-2 mt-2">
@@ -29,7 +27,6 @@ export default function page() {
             </ol>
           </div>
 
-          {/* why Choose us */}
           <div className="mt-6 sm:mt-8">
             <h1 className="text-lg sm:text-xl font-semibold">Why Choose Us?</h1>
             <ul className="list-disc list-inside space-y-1 mt-2">
@@ -40,7 +37,6 @@ export default function page() {
             </ul>
           </div>
 
-          {/* Join Us */}
           <div className="mt-6 sm:mt-8">
             <h1 className="text-lg sm:text-xl font-semibold">Join Us on This Journey.</h1>
             <p className="mt-2 text-sm sm:text-base">We invite you to become a part of our growing community of satisfied customers. Whether you're here to finding your dream car or exploring innovative solutions, we’re committed to making your experience unforgettable. Thank you for trusting "Car Dealing Experience" to be your partner in this journey.</p>
