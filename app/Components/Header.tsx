d4c49a4b78e92071cdfc76386c86354074e7f152
Abdul Raheem <raheemveer5@gmail.com>
2025-01-06 21:04:21 +0500

    create contact page

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -31,11 +31,11 @@ export default function Header() {
       <div className="block sm:hidden cursor-pointer"><i className="ri-menu-fold-fill text-xl font-bold" onClick={() => setIsNav(true)}></i></div>
       <nav className={`${isNav ? "z-[10] fixed left-[-10px] w-[250px] top-[20%] min-h-[400px] bg-blue-800 text-white flex flex-col items-center justify-center gap-6 py-10 rounded-md" : "hidden sm:flex gap-8"}`} onClick={() => setIsNav(false)} ref={navRef}>
         <div className="block sm:hidden absolute top-4 right-4 cursor-pointer" onClick={() => setIsNav(false)}><i className="ri-xrp-line text-white text-xl"></i></div>
-        <Link href="/" className="text-lg sm:text-base">Home</Link>
-        <Link href="/About"className="text-lg sm:text-base">About</Link>
-        <Link href="/Cars" className="text-lg sm:text-base">Cars</Link>
-        <Link href="/Blog" className="text-lg sm:text-base">Blog</Link>
-        <Link href="/Contact" className="text-lg sm:text-base">Contact</Link>
+        <Link href="/" className="text-lg sm:text-base py-[3px] sm:hover:text-blue-800 relative group">Home<span className="absolute left-0 bottom-0 w-0 h-[3px] bg-white sm:bg-blue-700 transition-all duration-300 group-hover:w-full"></span></Link>
+        <Link href="/About" className="text-lg sm:text-base py-[3px] sm:hover:text-blue-800 relative group">About<span className="absolute left-0 bottom-0 w-0 h-[3px] bg-white sm:bg-blue-700 transition-all duration-300 group-hover:w-full"></span></Link>
+        <Link href="/Cars" className="text-lg sm:text-base py-[3px] sm:hover:text-blue-800 relative group">Cars<span className="absolute left-0 bottom-0 w-0 h-[3px] bg-white sm:bg-blue-700 transition-all duration-300 group-hover:w-full"></span></Link>
+        <Link href="/Blog" className="text-lg sm:text-base py-[3px] sm:hover:text-blue-800 relative group">Blog<span className="absolute left-0 bottom-0 w-0 h-[3px] bg-white sm:bg-blue-700 transition-all duration-300 group-hover:w-full"></span></Link>
+        <Link href="/Contact" className="text-lg sm:text-base py-[3px] sm:hover:text-blue-800 relative group">Contact<span className="absolute left-0 bottom-0 w-0 h-[3px] bg-white sm:bg-blue-700 transition-all duration-300 group-hover:w-full"></span></Link>
       </nav>
     </header>
   );
